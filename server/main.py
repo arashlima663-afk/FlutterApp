f31a41bbb293e17d500feae498d356feba2f7c78
arashlima663-afk <arashlima663@gmail.com>
2026-01-16 17:07:08 +0100

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,52 +1,55 @@
 from cryptography.hazmat.primitives.asymmetric import x25519
 from cryptography.hazmat.primitives import serialization
 
-import uuid, os
+import uuid, os, asyncio
 from typing import List, Optional, Annotated
 
-from sqlalchemy import LargeBinary
-from sqlalchemy.orm import DeclarativeBase,Mapped, mapped_column, Session, sessionmaker
+from create_db import Database
+from fastapi import FastAPI, File, UploadFile 
+from fastapi.responses import PlainTextResponse, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-
-
-from fastapi import FastAPI, File, UploadFile
-
-
-app = FastAPI(title='Fast API',description='Flutter Client')
-
-UPLOAD_DIR = "uploaded_images"
+app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def generate_key_pair():
     private_key = x25519.X25519PrivateKey.generate()
     private_key_bytes = private_key.private_bytes(
                 encoding=serialization.Encoding.PEM,
                 format=serialization.PrivateFormat.PKCS8,
-                encryption_algorithm=serialization.NoEncryption())
+                encryption_algorithm=serialization.NoEncryption()).decode("utf-8").replace("-----BEGIN PRIVATE KEY-----", "").replace("-----END PRIVATE KEY-----", "").replace("\n", "")
 
     public_key = private_key.public_key()
     public_key_bytes = public_key.public_bytes(
                 encoding=serialization.Encoding.PEM,
-                format=serialization.PublicFormat.SubjectPublicKeyInfo)
-    
+                format=serialization.PublicFormat.SubjectPublicKeyInfo).decode("utf-8").replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "").replace("\n", "")
+
     return private_key_bytes, public_key_bytes
 
 
 
 
+@app.get("/keychange", response_class=JSONResponse)
+async def to_client():
+    private_key, public_key = generate_key_pair()
+    db = await Database.Connect()
+    await Database.Insert(db, pub_key= public_key, pv_key= private_key)
 
 
 
-@app.put("/")
-async def to_client(file: Annotated[bytes, File(description="A file read as bytes")]):
-
-    return 'to client'
+    return {'public_key':public_key}
+    
 
 @app.get("/")
 async def comes_to_server():
 
     return 'comes to server'
-
-
 
 
