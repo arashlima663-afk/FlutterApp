0b86ca074ee7d10116efdf48bb72ad709932f6b4
arashlima663-afk <arashlima663@gmail.com>
2026-01-16 17:07:20 +0100

    Update create_db.py

--- server/create_db.py
+++ server/create_db.py
@@ -1,13 +1,37 @@
-from db import engine, Keys, Base
-
 import asyncio
+import aiosqlite
 
+class Database:
+    async def Connect():
+        db = await aiosqlite.connect("database.db")
+        await db.execute("""
+            CREATE TABLE IF NOT EXISTS database (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                pub_key TEXT,
+                pv_key TEXT,
+                aes TEXT,
+                enc_photo TEXT
+            )
+        """)
+        await db.commit()
+        return db
 
-async def create_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    await engine.dispose()
+    async def Insert(db, pub_key, pv_key):
 
+        # columns = ", ".join(values.keys())
+        # placeholders = ", ".join("?" for _ in values)
+        sql = f"INSERT INTO database (pub_key, pv_key) VALUES (?, ?)"
 
+        await db.execute(sql, (pub_key, pv_key))
+        await db.commit()
 
-asyncio.run(create_db())
+    async def Read(db, **values):
+        c = db.cursor()
+
+        columns = ", ".join(values.keys())
+        await c.execute(f"SELECT {columns} FROM database")
+        records = await c.fetchall()
+        for i in records:
+            print(i)
+
+        await db.close()
